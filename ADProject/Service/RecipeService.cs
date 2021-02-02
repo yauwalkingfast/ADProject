13a7bd0ac5fb2d89d108514fe7393e2cf705a750
Tingkai911 <tingkai911@gmail.com>
2021-02-02 16:21:42 +0800

    connected to flask

--- ADProject/Service/RecipeService.cs
+++ ADProject/Service/RecipeService.cs
@@ -53,16 +53,6 @@ namespace ADProject.Service
 
         }
 
-        public async Task<Recipe> FindById(int? id)
-        {
-            var recipe = await _context.Recipes
-                .Include(r => r.User)
-                .Include(r => r.RecipeSteps)
-                .Include(r => r.RecipeIngredients)
-                .FirstOrDefaultAsync(m => m.RecipeId == id);
-            return recipe;
-        }
-
         public List<RecipeIngredient> FindRecipeStepsByRecipeId(int id)
         {
             List<RecipeIngredient> recipeIngredients = _context.RecipeIngredients.Where(
