ebca7b4929c9b0fff76a3afb13bf5102f659aa62
IsabelCHM <63800687+IsabelCHM@users.noreply.github.com>
2021-02-17 18:18:48 +0800

    AD bug fixes

---
+++ ADProject/JsonObjects/RecipePlusTags.cs
@@ -0,0 +1,18 @@
+﻿using ADProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADProject.JsonObjects
+{
+    public class RecipePlusTags
+    {
+        public RecipePlusTags() { }
+
+        public Recipe recipe { get; set; }
+
+        public string tags { get; set; }
+        //public RecipeTagListJson recipeTagList {​​ get; set; }​​
+    }
+}
